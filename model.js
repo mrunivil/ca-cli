d1b125cedb38e9ec75b0f9daa23c357e1b855c32
Loer <christopher.loer@atos.net>
2020-04-30 14:46:58 +0200

    Refactoring

--- model.js
+++ model.js
@@ -6,29 +6,29 @@ exports.create = (className) => {
     files.set();
 };
 
-templateModel = ({ className }) => `
-    import {AbstractModel} from '../../core/data/model/abstract.model';
+templateModel = ({ className }) =>
+    `import {AbstractModel} from '../../core/data/model/abstract.model';
 
-    export class ${className}Model extends AbstractModel {
-        readonly id:string;
+export class ${className}Model extends AbstractModel {
+    readonly id:string;
 
-        private constructor({id}:Partial<${className}Model>){
-            super();
-            this.id = id;
-        }
+    private constructor({id}:Partial<${className}Model>){
+        super();
+        this.id = id;
+    }
 
-        static fromJSON({id}:any):${className}Model{
-            return new ${className}Model({id});
-        }
+    static fromJSON({id}:any):${className}Model{
+        return new ${className}Model({id});
+    }
 
-        static toJSON({id}:Partial<${className}Model>):any{
-            return {
-                id
-            }
-        }
-
-        static from${className}Entity({id}:Partial<${className}Entity>):${className}Model{
-            return new ${className}Model({id});
+    static toJSON({id}:Partial<${className}Model>):any{
+        return {
+            id
         }
     }
+
+    static from${className}Entity({id}:Partial<${className}Entity>):${className}Model{
+        return new ${className}Model({id});
+    }
+}
 `;
